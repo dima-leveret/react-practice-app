e262a888025999cd20e7822ebb97f8acdb3e849d
Dmytro Zaichenko <64688465+dima-leveret@users.noreply.github.com>
2026-05-28 22:41:47 +0200

    init react practice app

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1>User searcher</h1>
+      </section>
+      <section>user input with search btn </section>
+    </>
+  );
+}
+
+export default App;
