7d6b64899ad9c502679d9ed060bf1ce7f6681430
Dmytro Zaichenko <64688465+dima-leveret@users.noreply.github.com>
2026-05-28 23:09:26 +0200

    add fetch users

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  console.log(users);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data.users));
+  }, []);
+
   return (
     <>
       <section>
