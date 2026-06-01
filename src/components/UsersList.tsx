b676d2361e38958baa2919a51e95f1a187469cd6
Dmytro Zaichenko <dimazayac@gmail.com>
2026-06-01 10:08:47 +0200

    add no results

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -3,14 +3,18 @@ import { type User, type UserListProps } from "../types";
 export const UsersList = ({ data, isLoading, error }: UserListProps) => {
   return (
     <>
-      {isLoading ?? <p>Loading...</p>}
-      {error ?? <p>{error}</p>}
-      {data?.map((user: User) => (
-        <div className="userContainer" key={user.id}>
-          <p>{user.firstName}</p>
-          <p>{user.email}</p>
-        </div>
-      ))}
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {data?.length === 0 ? (
+        <p>no results</p>
+      ) : (
+        data?.map((user: User) => (
+          <div className="userContainer" key={user.id}>
+            <p>{user.firstName}</p>
+            <p>{user.email}</p>
+          </div>
+        ))
+      )}
     </>
   );
 };
